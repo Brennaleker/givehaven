9d436fc153d11ed3a8c36cd4917178b49828c313
Brenna <brennaleker@gmail.com>
2015-10-29 21:21:31 -0700

    WIP - seed data

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20151020181959) do
     t.float    "total_requested"
     t.text     "organization_details"
     t.text     "project_details"
-    t.datetime "expires_on",           default: '2016-01-30 00:23:30'
+    t.datetime "expires_on",           default: '2016-01-30 04:17:22'
     t.datetime "created_at",                                                   null: false
     t.datetime "updated_at",                                                   null: false
   end
